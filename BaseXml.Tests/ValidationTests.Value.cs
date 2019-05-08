51561be1613c1ca8921551cc4db6c4418c2d4bcf
canro91 <canro91@hotmail.com>
2019-05-08 15:24:48 -0500

    Add tests for Value validator

--- BaseXml.Tests/ValidationTests.Value.cs
+++ BaseXml.Tests/ValidationTests.Value.cs
@@ -1,53 +1,14 @@
 ﻿using BaseXml.Validation;
 using FluentValidation.Results;
 using NUnit.Framework;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace BaseXml.Tests
 {
-    [TestFixture]
     public partial class DocumentTestsValidation
     {
         [Test]
-        public void Required_AnEmptyNode_IsInvalid()
-        {
-            var note = MakeNote(@"
-<?xml version=""1.0"" encoding=""utf-8""?>
-<note>
-  <from>Bob</from>
-  <to>Alice</to>
-  <subject>Subject</subject>
-  <body></body>
-</note>");
-            var validations = MakeValidator(new XPath("/note/body"), new Required());
-            var validator = new CheckDocument(validations);
-
-            ValidationResult results = validator.Validate(note);
-
-            Assert.IsFalse(results.IsValid);
-        }
-
-        [Test]
-        public void Required_ANonExistingNode_IsInvalid()
-        {
-            var note = MakeNote(@"
-<?xml version=""1.0"" encoding=""utf-8""?>
-<note>
-  <from>Bob</from>
-  <to>Alice</to>
-  <subject>Subject</subject>
-</note>");
-            var validations = MakeValidator(new XPath("/note/body"), new Required());
-            var validator = new CheckDocument(validations);
-
-            ValidationResult results = validator.Validate(note);
-
-            Assert.IsFalse(results.IsValid);
-        }
-
-        [Test]
-        public void Required_ANodeWithValue_IsValid()
+        public void Value_ANonEmptyNodeWithExpectedValue_IsValid()
         {
             var note = MakeNote(@"
 <?xml version=""1.0"" encoding=""utf-8""?>
@@ -57,7 +18,7 @@ namespace BaseXml.Tests
   <subject>Subject</subject>
   <body>Body</body>
 </note>");
-            var validations = MakeValidator(new XPath("/note/body"), new Required());
+            var validations = MakeValidator(new XPath("/note/subject"), new Value("Subject"));
             var validator = new CheckDocument(validations);
 
             ValidationResult results = validator.Validate(note);
@@ -66,18 +27,55 @@ namespace BaseXml.Tests
         }
 
         [Test]
-        public void Required_AnEmptyNode_AddsXPathInMessage()
+        public void Value_ANonEmptyNodeWithoutExpectedValue_IsInvalid()
         {
             var note = MakeNote(@"
 <?xml version=""1.0"" encoding=""utf-8""?>
 <note>
   <from>Bob</from>
   <to>Alice</to>
-  <subject>Subject</subject>
+  <subject>AValueDifferentFromSubject</subject>
+  <body>Body</body>
+</note>");
+            var validations = MakeValidator(new XPath("/note/subject"), new Value("Subject"));
+            var validator = new CheckDocument(validations);
+
+            ValidationResult results = validator.Validate(note);
+
+            Assert.IsFalse(results.IsValid);
+        }
+
+        [Test]
+        public void Value_AnEmptyNode_IsValid()
+        {
+            var note = MakeNote(@"
+<?xml version=""1.0"" encoding=""utf-8""?>
+<note>
+  <from>Bob</from>
+  <to>Alice</to>
+  <body>Body</body>
+</note>");
+            var validations = MakeValidator(new XPath("/note/subject"), new Value("Subject"));
+            var validator = new CheckDocument(validations);
+
+            ValidationResult results = validator.Validate(note);
+
+            Assert.IsTrue(results.IsValid);
+        }
+
+        [Test]
+        public void Value_ANonEmptyNodeWithoutExpectedValue_AddsXPathInMessage()
+        {
+            var note = MakeNote(@"
+<?xml version=""1.0"" encoding=""utf-8""?>
+<note>
+  <from>Bob</from>
+  <to>Alice</to>
+  <subject>AValueDifferentFromSubject</subject>
   <body></body>
 </note>");
-            var bodyXPath = "/note/body";
-            var validations = MakeValidator(new XPath(bodyXPath), new Required());
+            var bodyXPath = "/note/subject";
+            var validations = MakeValidator(new XPath(bodyXPath), new Value("Subject"));
             var validator = new CheckDocument(validations);
 
             ValidationResult results = validator.Validate(note);
@@ -85,20 +83,6 @@ namespace BaseXml.Tests
             var message = results.Errors.FirstOrDefault()?.ErrorMessage;
             Assert.IsNotNull(message);
             StringAssert.Contains(bodyXPath, message);
-        }
-
-
-        private Note MakeNote(string xml)
-        {
-            return new Note(xml.Trim());
-        }
-
-        private Dictionary<XPath, IList<IValidateNode>> MakeValidator(XPath xPath, IValidateNode validateNode)
-        {
-            return new Dictionary<XPath, IList<IValidateNode>>
-            {
-                { xPath, new List<IValidateNode> { validateNode } }
-            };
         }
     }
 }
