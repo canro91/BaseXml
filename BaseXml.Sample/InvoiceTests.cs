61774f698734fa31877969382329d0da91fa19a3
canro91 <canro91@hotmail.com>
2019-06-27 13:51:10 -0500

    Add sample to evaluate compound objects instead of just values

--- BaseXml.Sample/InvoiceTests.cs
+++ BaseXml.Sample/InvoiceTests.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
 
 namespace BaseXml.Sample
 {
@@ -28,6 +30,34 @@ namespace BaseXml.Sample
             var seller = invoice.Seller;
             Assert.AreEqual("Tony Starks", seller);
         }
+
+        [Test]
+        public void Invoice_MultipleProducts_EvaluateLines()
+        {
+            var xml = @"<Invoice>
+  <Billing Number=""1000"" Date=""2015-04-15"" Total=""2,000,000.00"" />
+  <Customer Name=""James Rhodey Rhodes"">
+    <Address AddressLine=""1-23 Main St."" City=""Los Angeles"" />
+  </Customer>
+  <Products>
+    <Product Code=""WAR-MACHINE-2"" Description=""IRON PATRIOT"" Quantity=""1"" UnitPrice=""1,000,000.00"" Total=""1,000,000.00"" />
+    <Product Code=""MARK VII"" Description=""Avengers"" Quantity=""1"" UnitPrice=""1,000,000.00"" Total=""1,000,000.00"" />
+  </Products>
+</Invoice>";
+            var invoice = new Invoice(xml);
+
+            var products = invoice.Products.ToList();
+
+            Assert.AreEqual(2, products.Count);
+
+            var product1 = products[0];
+            Assert.AreEqual("WAR-MACHINE-2", product1.Code);
+            Assert.AreEqual("1,000,000.00", product1.Price);
+
+            var product2 = products[1];
+            Assert.AreEqual("MARK VII", product2.Code);
+            Assert.AreEqual("1,000,000.00", product2.Price);
+        }
     }
 
     class Invoice : BaseDocument
@@ -52,10 +82,37 @@ namespace BaseXml.Sample
         public string Seller
             => Evaluate(new XPath("/Invoice/Supplier/@Seller"));
 
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                var products = new List<Product>();
+
+                var rawProducts = _xmlDocument.SelectNodes("/Invoice/Products/Product", _xmlNamespaceManager);
+                foreach (XmlNode node in rawProducts)
+                {
+                    var product = new Product
+                    {
+                        Code = node.SelectSingleNode("@Code", _xmlNamespaceManager)?.InnerText,
+                        Price = node.SelectSingleNode("@UnitPrice", _xmlNamespaceManager)?.InnerText
+                    };
+                    products.Add(product);
+                }
+
+                return products;
+            }
+        }
+
         public void AddSupplier(string contact, string seller)
         {
             var node = $@"<Supplier Contact=""{contact}"" Seller=""{seller}"" />";
             AddSiblingNodeAfterFirstOf(node, new XPath("/Invoice/Customer"));
         }
+    }
+
+    public class Product
+    {
+        public string Code { get; set; }
+        public string Price { get; set; }
     }
 }
