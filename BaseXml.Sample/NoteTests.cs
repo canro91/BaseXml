f0c7c807e49e1e378ec9d549ba9c94446a011bbd
canro91 <canro91@hotmail.com>
2019-11-02 12:39:09 -0500

    Add sample to evaluate node

--- BaseXml.Sample/NoteTests.cs
+++ BaseXml.Sample/NoteTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using BaseXml.Evaluation;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 
 namespace BaseXml.Sample
@@ -7,7 +9,7 @@ namespace BaseXml.Sample
     public class NoteTests
     {
         [Test]
-        public void ModifyNote_AnIncompleteNoteXml_LoadAndModifiesNote()
+        public void ModifyNote_AnIncompleteNoteXml_LoadsAndModifiesNote()
         {
             string xml = @"<?xml version=""1.0"" encoding=""utf-8""?>
 <note>
@@ -26,9 +28,43 @@ namespace BaseXml.Sample
             var ps = note.PS;
             Assert.AreEqual("I Love You", ps);
         }
+
+        [Test]
+        public void Evaluate_NoteXml_PopulatesPropertiesMappedToNodesAndAttributes()
+        {
+            var xml = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<note>
+  <metadata language=""en-US"">
+    <from>Bob</from>
+    <to>Alice</to>
+    <subject>Subject</subject>
+    <type>Memo</type>
+    <delivery>
+      <fromIp>127.0.0.1</fromIp>
+      <deliverAt>2019-01-01</deliverAt>
+    </delivery>
+  </metadata>
+  <body>Some Note content</body>
+</note>";
+            var note = new Note(xml);
+
+            var annotatedNote = note.EvaluateNode<Metadata>();
+
+            Assert.IsNotNull(annotatedNote);
+            Assert.AreEqual("en-US", annotatedNote.Language);
+            Assert.AreEqual("Bob", annotatedNote.From);
+            Assert.AreEqual("Alice", annotatedNote.To);
+            Assert.AreEqual("Subject", annotatedNote.Subject);
+            Assert.AreEqual(NoteType.Memo, annotatedNote.Type);
+            Assert.IsNotNull(annotatedNote?.Delivery);
+            Assert.AreEqual("127.0.0.1", annotatedNote.Delivery.FromIp);
+            Assert.AreEqual(new DateTime(2019, 1, 1), annotatedNote.Delivery.DeliverAt);
+
+            Assert.AreEqual("Some Note content", note.Body);
+        }
     }
 
-    class Note : BaseDocument
+    internal class Note : BaseDocument
     {
         public Note(string xml)
             : base(xml)
@@ -55,5 +91,42 @@ namespace BaseXml.Sample
             var psNode = $"<ps>{ps}</ps>";
             AddSiblingNodeAfterFirstOf(psNode, new XPath("/note/body"));
         }
+    }
+
+    [FromNode("metadata")]
+    internal class Metadata : INode
+    {
+        [FromAttr("language")]
+        public string Language { get; set; }
+
+        [FromNode("from")]
+        public string From { get; set; }
+
+        [FromNode("to")]
+        public string To { get; set; }
+
+        [FromNode("subject")]
+        public string Subject { get; set; }
+
+        [FromNode("type")]
+        public NoteType? Type { get; set; }
+
+        [FromNode("delivery")]
+        public Delivery Delivery { get; set; }
+    }
+
+    internal enum NoteType
+    {
+        Reminder,
+        Memo
+    }
+
+    internal class Delivery : INode
+    {
+        [FromNode("fromIp")]
+        public string FromIp { get; set; }
+
+        [FromNode("deliverAt")]
+        public DateTime DeliverAt { get; set; }
     }
 }
